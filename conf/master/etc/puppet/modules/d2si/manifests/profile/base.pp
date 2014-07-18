611c0f6074066f562700fb85d0749871980fba91
Thibaut Coutelou <Thibaut.Coutelou@ensimag.grenoble-inp.fr>
2014-07-18 14:12:44 +0200

    use serf to keep the load balancer members up to date

--- conf/master/etc/puppet/modules/d2si/manifests/profile/base.pp
+++ conf/master/etc/puppet/modules/d2si/manifests/profile/base.pp
@@ -1,6 +1,10 @@
 class d2si::profile::base{
 
-    package {unzip:
+    package {"unzip":
+        ensure  => present,
+    }
+
+    package{"curl":
         ensure  => present,
     }
 
@@ -13,6 +17,21 @@ class d2si::profile::base{
         config_hash => hiera_hash(serf::config_hash, {}),
     }
 
+    file{'/etc/serf/handlers/join.sh':
+        ensure  => 'file',
+        content => template('d2si/join.sh.erb'),
+        mode    => '755',
+    }
+
+    file{'/etc/serf/handlers/leave.sh':
+        ensure  => 'file',
+        content => template('d2si/leave.sh.erb'),
+        mode    => '755',
+    }
+
+    Service['serf'] <- File['/etc/serf/handlers/join.sh']
+    Service['serf'] <- File['/etc/serf/handlers/leave.sh']
     Class['serf']   <- User['serf']
     Class['serf']   <- Package['unzip']
+    Class['serf']   <- Package['curl']
 }
--- conf/master/etc/puppet/modules/d2si/manifests/profile/nginx_loadbalancer.pp
+++ conf/master/etc/puppet/modules/d2si/manifests/profile/nginx_loadbalancer.pp
@@ -1,34 +1,10 @@
 class d2si::profile::nginx_loadbalancer {
     include nginx
 
+    $upstream_name = hiera('nginx::resource::upstream::name')
     nginx::resource::vhost { 'appli':
         server_name => hiera('nginx::ressource::vhost::server_name'),
-        proxy       => 'http://farm',
+        proxy       => "http://$upstream_name",
     }
 
-    concat {"/etc/nginx/conf.d/farm-upstream.conf":
-        ensure  => present,
-        require => Package['nginx'],
-        notify  => Service['nginx'],
-    }
-
-    concat::fragment {'upstream_header':
-        target  => "/etc/nginx/conf.d/farm-upstream.conf",
-        content => "upstream farm {\n",
-        order  => "01",
-    }
-
-    concat::fragment {'upstream_footer':
-        target  => "/etc/nginx/conf.d/farm-upstream.conf",
-        content => "}\n",
-        order  => "03",
-    }
-
-    Concat::Fragment <<| |>>
-
-
-    #nginx::resource::upstream { 'farm':
-    #    ensure  => present,
-    #    members => hiera('nginx::resource::upstream::members'),
-    #}
 }
